8cbbf6a22fe9fff11c7511c3048d3afd51a4068b
DaltonDom <Domenighi5@gmail.com>
2023-02-10 15:16:22 -0600

    error and loader component

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,7 +2,8 @@ import { loader } from '../assets';
 
 const Loader = () => (
   <div className='w-full flex justify-center items-center flex-col'>
-    <img src={loader} alt="loader" className="w-32 h-3" />
+    <img src={loader} alt="loader" className="w-32 h-3 object-contain" />
+    <h1 className='font-bold text-2xl text-white mt-2'>{title || "Loading..."}</h1>
   </div>
 );
 
